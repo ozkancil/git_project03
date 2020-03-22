cda66e43d4ea9e816f8dc3eb29c5eaa5da2cc62b
ozkan <ozkancil92@gmail.com>
2020-03-22 12:55:30 -0400

    local branch first commit

--- src/gitDay4Package/GitDay4.java
+++ src/gitDay4Package/GitDay4.java
@@ -1,5 +1,11 @@
 package gitDay4Package;
 
 public class GitDay4 {
-
+public static void main(String[] args) {
+	System.out.println("local first commit");
+	System.out.println("we are in branch ozkan which is local branch first commit");
+	
+	
+	
+}
 }
