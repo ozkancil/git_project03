666be29a5df9f5b93aee5dd3cddfa1570d63a355
ozkancil <48811455+ozkancil@users.noreply.github.com>
2020-03-22 13:29:53 -0400

    changes from bahadir

--- src/gitDay4Package/GitDay4.java
+++ src/gitDay4Package/GitDay4.java
@@ -4,7 +4,7 @@ public class GitDay4 {
 public static void main(String[] args) {
 	System.out.println("local first commit");
 	System.out.println("we are in branch ozkan which is local branch first commit");
-	
+	System.out.println("Someone else contributed to the list");
 	
 	
 }
